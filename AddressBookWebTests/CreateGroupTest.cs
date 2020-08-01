0b54448a2ccf796623bef3b9f66a158c563cf15c
e.ivanchenko <e.ivanchenko@dodopizza.com>
2020-08-01 08:43:47 +0300

    Refactor CreateGroupTest

--- AddressBookWebTests/CreateGroupTest.cs
+++ AddressBookWebTests/CreateGroupTest.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium.Chrome;
 namespace AddressBookWebTests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class CreateGroupTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -23,7 +23,7 @@ namespace AddressBookWebTests
         }
 
         [TearDown]
-        public void TeardownTest()
+        public void TeardownTests()
         {
             try
             {
@@ -38,16 +38,35 @@ namespace AddressBookWebTests
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void CreateGroupTest()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.Id("LoginForm")).Submit();
+            OpenAuthPage();
+            Login();
+            GoToGroupsPage();
+            InitGroupCreation();
+            FillGroupForm();
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+            LogOut();
+        }
+
+        private void LogOut()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturnToGroupsPage()
+        {
             driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm()
+        {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
             driver.FindElement(By.Name("group_name")).SendKeys("Group name");
@@ -55,9 +74,30 @@ namespace AddressBookWebTests
             driver.FindElement(By.Name("group_header")).SendKeys("Group header");
             driver.FindElement(By.Name("group_footer")).Clear();
             driver.FindElement(By.Name("group_footer")).SendKeys("Group footer");
-            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
             driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void Login()
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys("secret");
+            driver.FindElement(By.Id("LoginForm")).Submit();
+        }
+
+        private void OpenAuthPage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
         }
 
         private bool IsElementPresent(By by)
